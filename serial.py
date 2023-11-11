d88d37eeeba3b0ed095e39a258f05473724fc4b4
Elijah Walker North Jones <elijahwalkernorthjones@gmail.com>
2023-11-10 16:43:41 -0800

    finished 1 of 4 problems

--- serial.py
+++ serial.py
@@ -20,3 +20,19 @@ class SerialGenerator:
     100
     """
 
+    def __init__(self, start=0):
+        self.start = self.next = start
+    
+    def __rep__(self):
+        """Show respresentation"""
+        return f"<SerialGenerator start={self.start} next={self.next}>"
+
+
+    def generate(self):
+        """Return Next Serial."""
+        self.next += 1
+        return self.next - 1
+    
+    def reset(self):
+        """Reset number to original starting point."""
+        self.next = self.start
